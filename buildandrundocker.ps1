2afe022d35110d027ac3277538b420b9349c1c32
Ryan Comer <ryancomer94@gmail.com>
2021-09-13 09:01:58 -0500

    Updated build script to make recordings mount configurable

--- buildandrundocker.ps1
+++ buildandrundocker.ps1
@@ -1,3 +1,19 @@
+# Name of the Docker container that is created
+$CONTAINER_NAME="voice-commands-bot"
 
+# OPTIONAL
+# Path for the recorded command.wav files
+# EXAMPLE: "C:\\Users\\UserName\\src\\discord\\voice-commands-bot\\recordings"
+$RECORDINGS_PATH = ""
+
+# Build the image
 docker build -t voice-commands-bot .
-docker run --name voice-commands-bot -v C:\\Users\\Ryan\\src\\discord\\voice-commands-bot\\recordings:/usr/src/voice-commands-bot/recordings:rw voice-commands-bot
+
+# Run the container
+# Check if the RECORDINGS_PATH variable is set
+if(($null -ne $RECORDINGS_PATH) -and ($RECORDINGS_PATH -ne "")){
+    docker run -d --name $CONTAINER_NAME -v ${RECORDINGS_PATH}:/usr/src/voice-commands-bot/recordings:rw voice-commands-bot
+}
+else{
+    docker run -d --name $CONTAINER_NAME voice-commands-bot
+}
