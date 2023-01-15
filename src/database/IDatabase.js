8b1cb876c7a9cb7959f8d0367f3dd9476d4ad6ed
Ryan Comer <ryancomer94@gmail.com>
2023-01-15 16:08:42 -0600

    Updated database code

--- src/database/IDatabase.js
+++ src/database/IDatabase.js
@@ -3,23 +3,31 @@ class IDatabase {
     constructor(){
         // Creates a new table in the database
         if(!this.CreateTable){
-            throw new Error('Commands must implement CreateTable(options)')
+            throw new Error('Database must implement CreateTable(options)')
         }
         // Adds a new row to the table
         if(!this.AddRow){
-            throw new Error('Commands must implement AddRow(options)')
+            throw new Error('Database must implement AddRow(options)')
+        }
+        // Adds multiple rows to the table
+        if(!this.AddRows){
+            throw new Error('Database must implement AddRows(options)')
         }
         // Gets a row from the table
         if(!this.GetRow){
-            throw new Error('Commands must implement GetRow(options)')
+            throw new Error('Database must implement GetRow(options)')
+        }
+        // Gets multiple rows from the table
+        if(!this.GetRows){
+            throw new Error('Database must implement GetRows(options)')
         }
         // Updates a row in the table
         if(!this.UpdateRow){
-            throw new Error('Commands must implement UpdateRow(options)')
+            throw new Error('Database must implement UpdateRow(options)')
         }
         // Deletes a row from the table
         if(!this.DeleteRow){
-            throw new Error('Commands must implement DeleteRow(options)')
+            throw new Error('Database must implement DeleteRow(options)')
         }
     }
 }
--- src/database/SqliteDatabase.js
+++ src/database/SqliteDatabase.js
@@ -42,10 +42,18 @@ class SqliteDatabase extends IDatabase {
         })
     }
 
+    // Add many rows to the database
+    AddRows(tableName, columns, values) {
+        // Call AddRow for each value
+        return Promise.all(values.map((value) => {
+            return this.AddRow(tableName, columns, value)
+        }))
+    }
+
     // Get a row in the database
     GetRow(tableName, columns, where) {
         return new Promise((resolve, reject) => {
-            const sql = `SELECT ${columns} FROM ${tableName}`
+            let sql = `SELECT ${columns} FROM ${tableName}`
 
             // Add the where clause if it exists
             if(where){
@@ -62,15 +70,87 @@ class SqliteDatabase extends IDatabase {
         })
     }
 
-    // Update a row in the database
-    UpdateRow(tableName, columns, values, where) {
+    // Get many rows in the database
+    GetRows(tableName, columns, where) {
         return new Promise((resolve, reject) => {
-            const sql = `UPDATE ${tableName} SET ${columns} = ${values}`
+            let sql = `SELECT ${columns} FROM ${tableName}`
             
             // Add the where clause if it exists
             if(where){
                 sql += ` WHERE ${where}`
             }
+
+            this.db.all(sql, (err, rows) => {
+                if(err){
+                    reject(err)
+                } else {
+                    resolve(rows)
+                }
+            })
+        })
+    }
+
+    // Update a row in the database
+    UpdateRow(tableName, columns, values, where) {
+        return new Promise((resolve, reject) => {
+            // Check if the columns and values are the same length
+            if(columns.length !== values.length){
+                reject(new Error('Columns and values must be the same length'))
+            }
+
+            // Build the set clause
+            let set = ''
+            for(let i = 0; i < columns.length; i++){
+                set += `${columns[i]} = ${values[i]}`
+                if(i < columns.length - 1){
+                    set += ', '
+                }
+            }
+
+            let sql = `UPDATE ${tableName} SET ${set}`
+            
+            // Add the where clause if it exists
+            if(where){
+                sql += ` WHERE ${where}`
+            }
+
+            this.db.run(sql, (err) => {
+                if(err){
+                    reject(err)
+                } else {
+                    resolve()
+                }
+            })
+        })
+    }
+
+    // Check if a table exists
+    TableExists(tableName) {
+        return new Promise((resolve, reject) => {
+            const sql = `SELECT count(*) FROM sqlite_master WHERE type='table' AND name='${tableName}'`
+            this.db.get(sql, (err, row) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    if (row && row['count(*)'] > 0) {
+                        resolve(true)
+                    } else {
+                        resolve(false)
+                    }
+                }
+            })
+        })
+    }
+
+    // Delete a row in the database
+    DeleteRow(tableName, where) {
+        // Check for where clause
+        if(!where){
+            throw new Error('DeleteRow requires a where clause')
+        }
+
+        return new Promise((resolve, reject) => {
+            const sql = `DELETE FROM ${tableName} WHERE ${where}`
 
             this.db.run(sql, (err) => {
                 if(err){
